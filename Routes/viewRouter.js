adcc0ec2891c5c48e0427b05413d2fe0deb2d3bf
Shardul Kurdukar <24shardul@gmail.com>
2022-03-27 02:05:38 +0530

    hoho

--- Routes/viewRouter.js
+++ Routes/viewRouter.js
@@ -1,13 +1,11 @@
-const {Router} = require("express");
+const { Router } = require("express");
 const authController = require("../controllers/authController");
 const viewController = require("../controllers/viewController");
 const userController = require("../controllers/userController");
 const router = Router();
 
-router.get("/login", viewController.getLogin);
+// router.get("/login", viewController.getLogin);
 router.get("/", viewController.getHome);
 router.get("/profile", viewController.getProfile); // add authController.isLoggedIn as a middleware
-
-
 
 module.exports = router;
