c5f304147408a1bc100afbd191d179a480b11584
saisuvanth <saisuvanth@gmail.com>
2022-03-27 01:46:28 +0530

    added pages

--- views/pages/home.js
+++ views/pages/home.js
@@ -5,11 +5,20 @@ window.addEventListener('resize',()=>{
 		removeOffCanvas();
 	}
 });
+window.addEventListener('load',()=>{
+	if(window.innerWidth < 768){
+		makeOffCanvas();
+	}
+});
+const filterDiv=document.querySelector('#filter-parent');
+const filterBut=document.querySelector('#filter-close-button');
+// const filterHead=document.querySelector('#mycol-1');
 
 function makeOffCanvas(){
-	const filterDiv=document.querySelector('#filter-parent');
 	filterDiv.id='offcanvasExample';
 	filterDiv.className='offcanvas offcanvas-start';
+	filterBut.style.display='block';
+	// filterHead.classList.add('offcanvas-header');
 }
 
 function removeOffCanvas(){
@@ -18,4 +27,6 @@ function removeOffCanvas(){
 	filterDiv.className='col-md-3 col-sm-0';
 	filterDiv.ariaHidden=false;
 	filterDiv.style.visibility='visible';
+	filterBut.style.display='none';
+	// filterHead.classList.remove('offcanvas-header');
 }
